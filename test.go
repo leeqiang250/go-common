5adc246b90d50112f62e62eafed1dc12ad11d8d6
leeqiang250 <leeqiang250@163.com>
2019-12-14 18:28:24 +0800

    BUG修复。

---
+++ test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func Test0() {
+	fmt.Println(time.Now().Unix(), "Test0")
+}
+
+func Test1() {
+	fmt.Println(time.Now().Unix(), "Test1")
+}
+
+func Test2() {
+	fmt.Println(time.Now().Unix(), "Test2")
+}
