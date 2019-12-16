b39d464e68a27d63966232d6732a07017ccb5712
leeqiang250 <leeqiang250@163.com>
2019-12-16 17:11:35 +0800

    BUG修复。

--- common/struct.go
+++ common/struct.go
@@ -3,10 +3,9 @@ package common
 import "sync"
 
 type Mutex struct {
-	Lock     *sync.Mutex
-	LockLock sync.Mutex
+	Lock sync.Mutex
 }
 
 type RWMutex struct {
-	Lock *sync.RWMutex
+	Lock sync.RWMutex
 }
